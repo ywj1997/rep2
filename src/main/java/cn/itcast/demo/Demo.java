e7963d3e38c256e98a839c461f3b7d51c3a63f90
yanweijin <582441912@qq.com>
2019-04-03 18:11:29 +0800

    更改Demo

--- src/main/java/cn/itcast/demo/Demo.java
+++ src/main/java/cn/itcast/demo/Demo.java
@@ -2,6 +2,7 @@ package cn.itcast.demo;
 
 public class Demo {
     public static void main(String[] args) {
+        System.out.println("更改-------");
         System.out.println("hello--------");
         System.out.println("hello--------");
         System.out.println("hello--------");
