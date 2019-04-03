3546ec02c6af0bc9e1df8a5ae5e73c682b48684e
yanweijin <582441912@qq.com>
2019-04-03 17:21:57 +0800

    添加helloPro项目

---
+++ src/main/java/cn/itcast/demo/Demo.java
@@ -0,0 +1,10 @@
+package cn.itcast.demo;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello--------");
+        System.out.println("hello--------");
+        System.out.println("hello--------");
+        System.out.println("hello--------");
+    }
+}
